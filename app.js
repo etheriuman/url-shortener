75b05bf7bf92429baa59a3f5a62bd28fa1b61b4d
etheriuman <timmy26291125@gmail.com>
2020-11-24 21:33:33 +0800

    add index page view

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // 載入插件
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 
 const app = express()
 
 const port = 3000
 
+// 設定樣板引擎
+app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
-
+// 首頁
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+// 送出短網址表單
+app.post('/shorten', (req, res) => {
+  // .....動作
 })
 
 // 啟動伺服器等待
