2a7d15b1d9f2a907e54c59d1e8fb782e6288c70f
etheriuman <timmy26291125@gmail.com>
2020-11-25 14:29:22 +0800

    update better code generate algorithm

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,8 +11,8 @@ const generateCode = require('../../public/javascripts/generateCode')
 const Url = require('../../models/url')
 
 
-// 解析 port 當前的狀態 決定要回傳那個 localhost
-const localhost = process.env.PORT ? 'https://polar-river-15707.herokuapp.com/shorten/' : 'http://localhost:3000/shorten/'
+// 解析 port 當前的狀態 決定要回傳那個 host
+const host = process.env.PORT ? 'https://polar-river-15707.herokuapp.com/shorten/' : 'http://localhost:3000/shorten/'
 
 
 // 設定路由：
@@ -36,38 +36,27 @@ router.post('/', (req, res) => {
          // 取出 url 的 code
          const code = url[0].code
          // 渲染畫面
-         res.render('index', { code, localhost })
+         res.render('index', { code,host })
        // 如果沒找到：
        } else {
          console.log('沒有找到匹配資料，生成一組code並開始進行後續動作！')
          // 創建一組 code
          const code = generateCode()
          // 尋找是否已經有相同 code 存在：
-         Url.find({ code: code })
-            .then(url => {
-              // 如果有找到：
-              if (url.length) {
-                console.log('找到重複的code，重新生成一組code並生成資料，渲染畫面！')
-                // 重新生成一組 code
+         Url.find()
+            .then(urls => {
+              // 確保 code 絕對不會與資料庫中的 code 相同
+              while (urls.some(url => url.code === code)) {
                 code = generateCode()
-                // 生成資料
-                Url.create({
-                      name: inputUrl,
-                      code: code
-                    })
-                   // 渲染畫面
-                   .then(() => res.render('index', { code, localhost }))
-              // 如果沒找到：
-              } else {
-                console.log('沒有找到重複的code，直接生成資料，渲染畫面！')
-                // 生成資料
-                Url.create({
-                      name: inputUrl,
-                      code: code
-                    })
-                    // 渲染畫面
-                   .then(() => res.render('index', { code, localhost }))
               }
+              // 生成資料
+              Url.create({
+                    name: inputUrl,
+                    code: code
+                  })
+                  // 渲染畫面
+                  .then(() => res.render('index', { code, host }))
+              
             })
        }
      })
