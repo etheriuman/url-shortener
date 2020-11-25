e88d5337f25e45d30fdcb7e369c7cf473111d360
etheriuman <timmy26291125@gmail.com>
2020-11-25 23:18:09 +0800

    code formatting

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -29,37 +29,37 @@ router.post('/', (req, res) => {
   console.log('this is ' + inputUrl)
   // 尋找db是否有該url的項目
   Url.find({ name: inputUrl })
-     .then(url => {
-       // 如果有找到：
-       if (url.length) {
-         console.log('找到了匹配資料，渲染畫面！')
-         // 取出 url 的 code
-         const code = url[0].code
-         // 渲染畫面
-         res.render('index', { code,host })
-       // 如果沒找到：
-       } else {
-         console.log('沒有找到匹配資料，生成一組code並開始進行後續動作！')
-         // 創建一組 code
-         const code = generateCode()
-         // 尋找是否已經有相同 code 存在：
-         Url.find()
-            .then(urls => {
-              // 確保 code 絕對不會與資料庫中的 code 相同
-              while (urls.some(url => url.code === code)) {
-                code = generateCode()
-              }
-              // 生成資料
-              Url.create({
-                    name: inputUrl,
-                    code: code
-                  })
-                  // 渲染畫面
-                  .then(() => res.render('index', { code, host }))
-              
+    .then(url => {
+      // 如果有找到：
+      if (url.length) {
+        console.log('找到了匹配資料，渲染畫面！')
+        // 取出 url 的 code
+        const code = url[0].code
+        // 渲染畫面
+        res.render('index', { code, host })
+        // 如果沒找到：
+      } else {
+        console.log('沒有找到匹配資料，生成一組code並開始進行後續動作！')
+        // 創建一組 code
+        const code = generateCode()
+        // 尋找是否已經有相同 code 存在：
+        Url.find()
+          .then(urls => {
+            // 確保 code 絕對不會與資料庫中的 code 相同
+            while (urls.some(url => url.code === code)) {
+              code = generateCode()
+            }
+            // 生成資料
+            Url.create({
+              name: inputUrl,
+              code: code
             })
-       }
-     })
+          // 渲染畫面
+          .then(() => res.render('index', { code, host }))
+
+          })
+      }
+    })
 })
 
 
